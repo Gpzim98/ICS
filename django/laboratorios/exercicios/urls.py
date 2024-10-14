50072210cd07890675d5aadae815da2a9cc79e43
gpzim98 <gpzim98@gmail.com>
2024-10-14 11:54:20 +0100

    Concluindo o exercicio 2

--- django/laboratorios/exercicios/urls.py
+++ django/laboratorios/exercicios/urls.py
@@ -5,4 +5,5 @@ import exercicios.views
 urlpatterns = [
     path('', exercicios.views.home, name='home'),
     path('ex1/', exercicios.views.ex1, name='ex1'),
+    path('ex2/', exercicios.views.ex2, name='ex2'),
 ]
--- django/laboratorios/exercicios/views.py
+++ django/laboratorios/exercicios/views.py
@@ -13,3 +13,18 @@ def ex1(request):
         'frase': frase
     }
     return render(request, 'ex1.html', data)
+
+def ex2(request):
+    data = {
+        'titulo': 'Exercicio 2. Calculo de total',
+        'descricao_exercicio': 'Faca um programa que receba 2 numeros do usuario e calcule o total',
+    }
+
+    if request.method == 'POST':
+        num1 = request.POST.get('num1')
+        num2 = request.POST.get('num2')
+        total = int(num1) + int(num2)
+
+        data['total'] = total
+    
+    return render(request, 'ex2.html', data)
