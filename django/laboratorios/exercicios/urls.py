816d71f5642d3da3639253fa210dc3826e307e06
gpzim98 <gpzim98@gmail.com>
2024-10-19 13:08:23 +0100

    delete

--- django/laboratorios/exercicios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-import exercicios.views
-
-urlpatterns = [
-    path('', exercicios.views.home, name='home'),
-    path('ex1/', exercicios.views.ex1, name='ex1'),
-    path('ex2/', exercicios.views.ex2, name='ex2'),
-]
